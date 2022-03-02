b34a07e27e54e6de1f4fb03659e5240fdcc98194
adriantorralbagomez <adriantorralba1@gmail.com>
2022-03-02 16:49:58 +0100

    bd local

--- config/db.php
+++ config/db.php
@@ -2,9 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=alum1.iesfsl.org;dbname=uvademesa',
-    'username' => '2daw',
-    'password' => 'fonteta',
+    // 'dsn' => 'mysql:host=alum1.iesfsl.org;dbname=uvademesa',
+    // 'username' => '2daw',
+    // 'password' => 'fonteta',
+    'dsn' => 'mysql:host=localhost;dbname=uvademesa',
+    'username' => 'adrian',
+    'password' => 'adrian',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
