327acc8c319982d31ab3b3e85281f6cb6e350106
adriantorralbagomez <adriantorralba1@gmail.com>
2022-05-23 19:15:44 +0200

    menus

--- views/layouts/_menuG.php
+++ views/layouts/_menuG.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    ["label" => "Inicio", "url" => "#", "icon" => "home"],
+    ["label" => "Inicio", "url" => "/#", "icon" => "home"],
     ['label' => 'Materiales (stock)', 'url' => ['/material'], "icon" => "table"],
     ['label' => 'Proveedores', 'url' => ['/proveedor'], "icon" => "address-book"],
     ['label' => 'Proveedores - Material', 'url' => ['/proveedor-material'], "icon" => "clipboard"],
